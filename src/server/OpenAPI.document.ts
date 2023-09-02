2444b4bf236a6f3718a32c8b1406f3ea7b0ec686
Alan Jones Rios <alan.jones.rios@gmail.com>
2023-09-02 22:40:21 +0000

    chore: configurações do servidor

--- src/server/OpenAPI.document.ts
+++ src/server/OpenAPI.document.ts
@@ -6,5 +6,5 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "tRPC OpenAPI",
   description: "OpenAPI compliant REST API built using tRPC with Next.js",
   version: "1.0.0",
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL+"/api",
 });
