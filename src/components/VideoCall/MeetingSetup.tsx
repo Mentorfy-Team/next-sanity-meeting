57315e7099058db62783460ecd49cad133cfe2f9
Alan Jones Rios <alan.jones.rios@gmail.com>
2024-10-21 19:45:08 -0300

    fix: correção na criação da sala

--- src/components/VideoCall/MeetingSetup.tsx
+++ src/components/VideoCall/MeetingSetup.tsx
@@ -47,7 +47,8 @@ const MeetingSetup = ({
       </div>
       <Button
         className="rounded-md bg-green-500 px-4 py-2.5"
-        onClick={() => {
+        onClick={async () => {
+          await call.getOrCreate();
           call.join();
           setIsSetupComplete(true);
         }}
--- src/hooks/userStore.ts
+++ src/hooks/userStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 interface UserState {
+  id: string;
   name: string;
   email: string;
   setName: (name: string) => void;
